2e111ab9bcb9a061203fccc55f16f966a4b6591d
unknown <ocesaralmeida@gmail.com>
2019-01-13 10:51:36 -0200

    adding new Classes to the project and studying Arrays

--- java6-projetos-iniciais.zip_expanded/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ java6-projetos-iniciais.zip_expanded/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,25 +1,33 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-//		/nt[] idades = new int[5];
-		ContaCorrente[] contas = new ContaCorrente[5];
+//		int[] idades = new int[5];
+		Object[] referencias = new Object[5];
+		
 		ContaCorrente cc1 = new ContaCorrente(11, 22);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(22, 33);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(22, 33);
+		referencias[1] = cc2;
 		
-		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+			
 //		System.out.println(cc2.getNumero());
 		
-		System.out.println(contas[1].getNumero());
+//		Object referenciaGenerica = referencias[1];
+//		
+//		System.out.println(referenciaGenerica.getNumero());
 		
-		ContaCorrente ref = contas[1];
+		ContaPoupanca ref = (ContaPoupanca) referencias[1];//type cast
 		System.out.println(cc2.getNumero());
 		System.out.println(ref.getNumero());
 
